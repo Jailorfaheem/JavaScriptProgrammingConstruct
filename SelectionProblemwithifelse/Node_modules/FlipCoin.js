2a7d886d8f13fbcec274983ec152fb3c206d4bb0
faheem <lalafahim55@gmail.com>
2021-12-05 21:15:49 +0530

    faheem created this for programming construct

---
+++ SelectionProblemwithifelse/Node_modules/FlipCoin.js
@@ -0,0 +1,11 @@
+let randomNumber = Math.floor(( Math.random() * 2 ) + 1);
+
+//if else condition for checking head or tail
+if ( randomNumber == 1 ) 
+{
+    console.log("HEAD");
+}
+else
+{
+    console.log("TAIL");
+}
